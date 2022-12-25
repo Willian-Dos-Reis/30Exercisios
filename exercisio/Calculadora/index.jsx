6ea2ef544f31867025aa0278f5bb5924f4ae7a66
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-12-25 11:43:36 -0300

    calculadora

--- exercisio/Calculadora/index.jsx
+++ exercisio/Calculadora/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import './style.css'
 
 function Calculadora() {
 
 
   return (
-    <div>
-      <p>ola will</p>
-      <h1>helo word</h1>
+    <div className='bt'>
+      <div className='res' >res</div>
+      <button>C</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button className='yellon' >/</button><br />
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button className='yellon' >X</button><br />
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button className='yellon'>-</button><br />
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button className='yellon'>+</button><br />
+      <button className='zero' >0</button>
+      <button>,</button>
+      <button className='yellon'>=</button>
     </div>
     
   )
--- exercisio/main.jsx
+++ exercisio/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Calculadora from './Calculadora/index.jsx'
-import App from './app.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Calculadora></Calculadora>
-    <App></App>
   </React.StrictMode>,
 )
