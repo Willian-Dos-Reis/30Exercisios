3159da0ea06ff65b620c4a6073add504d66fd8f7
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-12-29 22:40:22 -0300

    calculadora

--- exercisio/Calculadora/index.jsx
+++ exercisio/Calculadora/index.jsx
@@ -3,6 +3,8 @@ import './style.css'
 
 function Calculadora() {
     const [num, setNum]=useState(0)
+    const [numeroAntigo, setnumeroAntigo] =useState(0)
+    const [operador, setOperador] =useState(0)
     
     function inputNum(e){
       var input = e.target.value
@@ -21,9 +23,38 @@ function Calculadora() {
       setNum(0)
     }
 
-    function soma(){
-      setNum(num+num)
+    function mm(){
+      if (num>0){
+       setNum(-num) 
+      }else{
+        setNum(Math.abs(num))
+      }
+      
+    }
+    function dados(e){
+      var operadorInput = e.target.value
+      setOperador(operadorInput);
+      setnumeroAntigo(num);
+      setNum(0);
+    }
 
+
+    function calcular(){
+      if(operador === "/")
+      setNum(numeroAntigo / num)
+
+      else if (operador === "+"){
+        setNum(parseFloat(numeroAntigo) + parseFloat(num))
+
+      }
+      else if (operador === "-"){
+        setNum (numeroAntigo - num)
+      }
+      
+      else if (operador === "x"){
+        setNum(numeroAntigo * num)
+
+      }
     }
   
 
@@ -31,24 +62,24 @@ function Calculadora() {
     <div className='bt'>
       <h1>{num}</h1>
       <button onClick={apagar} >C</button>
-      <button >+/-</button>
+      <button  onClick={mm}  >+/-</button>
       <button onClick={porcentagem}>%</button>
-      <button className='yellon'  >/</button><br />
+      <button className='yellon' onClick={dados} value={"/"} >/</button><br />
       <button onClick={inputNum} value={7} >7</button>
       <button onClick={inputNum} value={8} >8</button>
       <button onClick={inputNum} value={9} >9</button>
-      <button className='yellon' >X</button><br />
+      <button className='yellon' onClick={dados} value={"x"} >X</button><br />
       <button onClick={inputNum} value={4} >4</button>
       <button onClick={inputNum} value={5} >5</button>
       <button onClick={inputNum} value={6} >6</button>
-      <button className='yellon'> - </button><br />
+      <button className='yellon' onClick={dados} value={"-"} > - </button><br />
       <button onClick={inputNum} value={1} >1</button>
       <button onClick={inputNum} value={2} >2</button>
       <button onClick={inputNum} value={3} >3</button>
-      <button className='yellon' onClick={soma} >+</button><br />
+      <button className='yellon' onClick={dados} value={"+"} >+</button><br />
       <button onClick={inputNum} value={0}  className='zero' >0</button>
-      <button>,</button>
-      <button className='yellon'>=</button>
+      <button onClick={inputNum} value={","}>,</button>
+      <button className='yellon' onClick={calcular}>=</button>
     </div>
     
   )
