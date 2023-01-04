0927fffe72dff8b6a89e0e79e7229b6db64f3f5f
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-01-04 18:36:23 -0300

    asd

--- exercisio/Lista-Tarefas/index.jsx
+++ exercisio/Lista-Tarefas/index.jsx
@@ -1,17 +1,40 @@
 import { useState } from 'react';
 import  './style.css'
-import {IoMdAdd, IoMdAlert} from 'react-icons/io'
+import {IoMdAdd, IoMdAlert, IoMdClose} from 'react-icons/io'
+import { ToastContainer,toast} from 'react-toastify';
+import 'react-toastify/dist/React-toastify.css'
 
 
 function ListaTarefas(){
+
+    const [task,setTask] = useState('')
+
+    //para armazenar todas taks criadas no input em um array//
+    const [taks,setTasks] = useState([])
+
+    function HanderCreateTask(){
+        if(taks === ''){
+            //erro
+        }else{
+            //adiciona
+        }
+
+    }
+ 
+
+
 return (
     <div className='app' >
+        <ToastContainer></ToastContainer>
        
        <div className='todo'>
 
           <header>
-            <input type="text"/>
-            <button><IoMdAdd></IoMdAdd></button>
+            <input type="text"
+            value={task}
+            onChange={(ev)=>setTask(ev.target.value)}
+            />
+            <button onClick={HanderCreateTask} ><IoMdAdd></IoMdAdd></button>
           </header>
 
             <div className='task-container' >
@@ -26,7 +49,7 @@ return (
                 </div>
 
                 <div>
-                    <IoMdAlert></IoMdAlert>
+                    <IoMdClose></IoMdClose>
                 </div>
 
                 
