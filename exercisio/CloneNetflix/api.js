7fffbad017fff4dd2b45225cfe3a924925b1f707
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-01-07 00:44:47 -0300

    sdf

--- exercisio/CloneNetflix/api.js
+++ exercisio/CloneNetflix/api.js
@@ -15,5 +15,41 @@ const categorias = [
         path : "/discover/tv?api_key=${API_KEY}&with_networks=213",
 
     },
+    {
+        name : "topRated",
+        title: "Populares",
+        path : "/move/top_rated?api_key=${API_KEY}&language=pt-BR",
+
+    },
+    {
+        name : "comedy",
+        title: "Comédias",
+        path : "/discover/tv?api_key=${API_KEY}&with_genres=35",
+
+    },
+    {
+        name : "Romances",
+        title: "Romances",
+        path : "/discover/tv?api_key=${API_KEY}&with_genres=1074",
+
+    },
+    {
+        name : "documentaries",
+        title: "Documentarios",
+        path : "/discover/tv?api_key=${API_KEY}&with_genres=99",
+
+    },
     
 ]
+
+export const getMovies = async (path) =>{
+    //const fazendo a requisição
+    let url = 'https://api.themoviedb.org/3/movie/550?api_key=6f18e7b560e9d377244216ad6b35d8a2'
+    
+    try {
+
+    } catch(erro){ //caso der erro ele emvia pro console e informa 
+        console.log("erro getMovies " , erro)
+        
+    }
+}
