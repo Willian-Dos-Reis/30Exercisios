595a555fa7f95ef1aaf21a3db55a179a588c57d7
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2023-01-13 22:14:03 -0300

    sdf

--- exercisio/main.jsx
+++ exercisio/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import Calculadora from './Calculadora/index.jsx'
 import CloneNetflix from './CloneNetflix'
+//import Lista from './Lista-Tarefas/index'
+import Test from './test/index'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <CloneNetflix></CloneNetflix>
+    <Test></Test>
   </React.StrictMode>,
 )
---
+++ exercisio/test/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+export default function index() {
+
+    const [nome,setNome] = useState ('')
+    const [nomes,setNomes] = useState ([])
+
+    function criar(){
+        const newName = {
+            title:nome,
+        }
+        setNomes([...nomes,newName])
+        setNome('')
+    }
+
+  return (
+    <div>
+        <input type="text"
+         placeholder='escreva'
+         onChange={(e)=>setNome(e.target.value)}
+         value={nome}
+         />
+        <button onClick={criar} >enter</button>
+
+        {nomes.map(nome=>(
+            <div>
+                <h1>{nome.title}</h1>
+            </div>
+        ))
+
+        }
+
+
+    </div>
+  )
+}
